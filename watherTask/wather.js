ca03e264ad898ca59b9589b4d0f945b15383db02
Wadimx199789 <martinkewichwadim@gmail.com>
2020-08-28 01:15:49 +0300

    add wather application

---
+++ watherTask/wather.js
@@ -0,0 +1,16 @@
+let object={};
+let key='bcdb7d11bef7e73df9c5';
+object.button=document.querySelector(".form__button");
+object.input=document.querySelector(".form__input");
+object.container=document.querySelector(".container");
+
+function checkWather(){
+    let name=object.input.value;
+    fetch('http://api.openweathermap.org/data/2.5/weather?q=`${name}`&appid=ccf36f09878b2f36d7c6a69e31fb8bbc')
+    .then(function(resp){return resp.json()})
+    .then(function(data){
+        console.log(data);
+    })
+    
+}
+object.button.addEventListener("click",checkWather);
