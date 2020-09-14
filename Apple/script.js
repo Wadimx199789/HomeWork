ba38f96fdb7cff66bac3bb187054719e13451412
Wadimx199789 <martinkewichwadim@gmail.com>
2020-09-15 01:17:05 +0300

    add redirectFunction

--- Apple/script.js
+++ Apple/script.js
@@ -1,4 +1,7 @@
 var eye=document.querySelector('.password-control');
+var button=document.querySelector('.form-submit');
+var password = document.getElementById("password-input");
+var user=document.getElementById("input-name");
 
 function show_hide(){
 var password = document.getElementById("password-input");
@@ -11,4 +14,19 @@ else{
     password.setAttribute('type','password');
 }
 }
+function redirect(){
+    if(user.value == "Admin" && password.value == "12345"){
+        window.location.href ="index.html";
+    }
+    else{
+        console.log("error");
+    }
+
+}
+button.addEventListener("keypress",e=>{
+    if (e.keyCode === 13) {
+         redirect();
+        e.preventDefault();
+    }
+}, false);
 eye.addEventListener('click',show_hide);
